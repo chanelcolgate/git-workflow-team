9d9dfa1ae688964400ea35cb314614f4b4034169
naga <thanhdatbkdn14@gmail.com>
2022-07-24 16:25:41 +0700

    #5 - chanelcolgate modify src/model.py

--- src/model.py
+++ src/model.py
@@ -12,42 +12,115 @@ class Sample:
 		sepal_width: float,
 		petal_length: float,
 		petal_width: float,
-		species: Optional[str] = None,
 	) -> None:
 		self.sepal_length = sepal_length
 		self.sepal_width = sepal_width
 		self.petal_length = petal_length
 		self.petal_width = petal_width
-		self.species = species
-		self.classification: Optional[str] = None
 
 	def __repr__(self) -> str:
-		if self.species is None:
-			known_unknown = "UnknownSample"
-		else:
-			known_unknown = "KnownSample"
-		
-		if self.classification is None:
-			classification = ""
-		else:
-			classification = f", classification={self.classification!r}"
-
 		return (
-			f"{known_unknown}("
+			f"{self.__class__.__name__}("
 			f"sepal_length={self.sepal_length}, "
 			f"sepal_width={self.sepal_width}, "
 			f"petal_length={self.petal_length}, "
 			f"petal_width={self.petal_width}, "
-			f"species={self.species!r}"
-			f"{classification}"
 			f")"
 		)
 
-	def classify(self, classification: str) -> None:
+class KnownSample(Sample):
+	"""Abstract superclass for testing and training data, the species is set externally."""
+	def __init__(
+		self,
+		species: str,
+		sepal_length: float,
+		sepal_width: float,
+		petal_length: float,
+		petal_width: float,
+	) -> None:
+		super().__init__(
+			sepal_length=sepal_length,
+			sepal_width=sepal_width,
+			petal_length=petal_length,
+			petal_width=petal_width,
+		)
+		self.species = species
+
+	def __repr__(self) -> str:
+		return (
+			f"{self.__class__.__name__}("
+			f"sepal_length={self.sepal_length}, "
+			f"sepal_width={self.sepal_width}, "
+			f"petal_length={self.petal_length}, "
+			f"petal_width={self.petal_width}, "
+			f"species={self.species!r}, "
+			f")"
+		)
+
+class TrainingKnownSample(KnownSample):
+	"""Training data."""
+	pass
+
+class TestingKnownSample(KnownSample):
+	"""Testing data. A classifier can assign a species, which may or may not be correct."""
+	def __init__(
+		self,
+		species: str,
+		sepal_length: float,
+		sepal_width: float,
+		petal_length: float,
+		petal_width: float,
+		classification: Optional[str] = None,
+	) -> None:
+		super().__init__(
+			species=species,
+			sepal_length=sepal_length,
+			sepal_width=sepal_width,
+			petal_length=petal_length,
+			petal_width=petal_width,
+		)
 		self.classification = classification
 
 	def matches(self) -> bool:
 		return self.species == self.classification
+
+	def __repr__(self) -> str:
+		return (
+			f"{self.__class__.__name__}("
+			f"sepal_length={self.sepal_length}, "
+			f"sepal_width={self.sepal_width}, "
+			f"petal_length={self.petal_length}, "
+			f"petal_width={self.petal_width}, "
+			f"species={self.species!r}, "
+			f"classification={self.classification!r}, "
+			f")"
+		)
+
+class UnknownSample(Sample):
+	"""A sample provided by a User, not yet classified."""
+	pass
+
+class ClassifiedSample(Sample):
+	"""Created from a sample provided by a User, and the results of classification."""
+	def __init__(self, classification: str, sample: UnknownSample) -> None:
+		super().__init__(
+			sepal_length=sample.sepal_length,
+			sepal_width=sample.sepal_width,
+			petal_length=sample.petal_length,
+			petal_width=sample.petal_width,
+		)
+		self.classification = classification
+
+	def __repr__(self) -> str:
+		return (
+			f"{self.__class__.__name__}("
+			f"sepal_length={self.sepal_length}, "
+			f"sepal_width={self.sepal_width}, "
+			f"petal_length={self.petal_length}, "
+			f"petal_width={self.petal_width}, "
+			f"classification={self.classification!r}, "
+			f")"
+		)
 
 class Hyperparamter:
 	"""
