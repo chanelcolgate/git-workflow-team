4f01a27204f89812f8dd5e09421edd78aae00d68
naga <thanhdatbkdn14@gmail.com>
2022-07-24 14:51:36 +0700

    #1 - channelcolgate update model.py

---
+++ src/model.py
@@ -0,0 +1,44 @@
+"""
+Chapter 2. Case Study
+"""
+from collections.abc import Iterator
+import datetime
+from typing import Optional, Union, Iterable
+
+class Sample:
+	def __init__(
+		self,
+		sepal_length: float,
+		sepal_width: float,
+		petal_length: float,
+		petal_width: float,
+		species: Optional[str] = None,
+	) -> None:
+		self.sepal_length = sepal_length
+		self.sepal_width = sepal_width
+		self.petal_length = petal_length
+		self.petal_width = petal_width
+		self.species = species
+		self.classification: Optional[str] = None
+
+	def __repr__(self) -> str:
+		if self.species is None:
+			known_unknown = "UnknownSample"
+		else:
+			known_unknown = "KnownSample"
+		
+		if self.classification is None:
+			classification = ""
+		else:
+			classification = f", classification={self.classification!r}"
+
+		return (
+			f"{known_unknown}("
+			f"sepal_length={self.sepal_length}, "
+			f"sepal_width={self.sepal_width}, "
+			f"petal_length={self.petal_length}, "
+			f"petal_width={self.petal_width}, "
+			f"species={self.species!r}"
+			f"{classification}"
+			f")"
+		)
